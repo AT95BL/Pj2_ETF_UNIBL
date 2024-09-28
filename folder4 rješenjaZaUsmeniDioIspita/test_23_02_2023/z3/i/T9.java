3e1ef1f0f758a152aa950f2cedf0630b0a66f1ee
Andrej Trožić <andrejtrozic95@gmail.com>
2024-09-28 11:35:32 +0200

    update..
    1. i 3. zadatak su finished ..

--- folder4 rješenjaZaUsmeniDioIspita/test_23_02_2023/z3/i/T9.java
+++ folder4 rješenjaZaUsmeniDioIspita/test_23_02_2023/z3/i/T9.java
@@ -9,9 +9,27 @@ return val*val;
 }
 public static void main(String[] args) {
 for(T9 t5: T9.values()){
-System.out.println(t5);
-System.out.println(t5.ordinal());
-System.out.println(t5.val2());
+System.out.println(t5);                 //  ispisuje simbol
+System.out.println(t5.ordinal());       //  ispisuje redni broj simbola u listi enumeracija, brojanje počinje od 0
+System.out.println(t5.val2());          //  poziva metodu koja kvadrira eksplicitno zadatu numeričku vrijednost simbola..
 }
 }
 }
+
+/*
+ *  A
+ *  0
+ *  1
+ *  B
+ *  1
+ *  4
+ *  C
+ *  2
+ *  9
+ *  D
+ *  3
+ *  16
+ *  E
+ *  4
+ *  16
+ */
