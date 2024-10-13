1e8f3a372896aa107daa52bf082ea62143869daf
Andrej Trožić <andrejtrozic95@gmail.com>
2024-10-13 16:39:04 +0200

    update
    resume day ..

--- folder1 randomIspitniZadaci/pj2Practice/practice 0 proba/Proba1.java
+++ folder1 randomIspitniZadaci/pj2Practice/practice 0 proba/Proba1.java
@@ -6,7 +6,7 @@ public class Proba1{
 	
 	public static void main(String[] args){
 		List list = new LinkedList();			//	<=> List<Object> list = new LinkedList();
-		list.add(new Integer(1));			//	<=> tipa sa Object o = new Integer(1);
+		list.add(new Integer(1));				//	<=> tipa sa Object o = new Integer(1);
 		Integer i = list.iterator().next();		//	<=> Integer i = ko Zna Šta!?
 												//		Integer i = (Integer)list.iterator().next(); resolves compiler warning	
 		System.out.println(i);
