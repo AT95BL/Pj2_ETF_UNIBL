f311ae16b6efdb811ea63817d1afcc2e26b03883
Andrej Trožić <andrejtrozic95@gmail.com>
2024-10-30 16:45:59 +0100

    Main class update ..

--- folder2 riješeniPismeniIspitniRokovi/pj2_16_06_2021/Zad3/Main.java
+++ folder2 riješeniPismeniIspitniRokovi/pj2_16_06_2021/Zad3/Main.java
@@ -1,14 +1,29 @@
+
 import java.nio.file.*;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main
 {
 	public static void main(String[]args)throws IOException
 	{
-		Path path=Paths.get("C:\\Users\\pc\\Desktop\\PJ2backup\\PJ2 zadaci novo");
-		Path outPath=Paths.get("C:\\Users\\pc\\Desktop\\test");
-		Finder finder=new Finder(".txt",outPath);
-		Files.walkFileTree(path,finder);							//	početni direktorijum, implementacija FileVisitor interface-a 
+		Scanner scanner = new Scanner(System.in);
+		String extension = "";
+		String line = "";
+
+		System.out.println("Unesite pocetnu putanju: ");
+		line = scanner.nextLine();
+		Path pocetnaPutanja = Paths.get(line);
+
+		System.out.println("Unesite destinacionu putanju: ");
+		line = scanner.nextLine();
+		Path destinacionaPutanja = Paths.get(line);
+
+		System.out.println("Unesite ekstenziju: ");
+		extension = scanner.nextLine();
+
+		Finder finder=new Finder(extension,destinacionaPutanja);
+		Files.walkFileTree(pocetnaPutanja,finder);							//	početni direktorijum, implementacija FileVisitor interface-a 
 		System.out.println(finder.getNumOfMatchedFiles());
 		finder.copyAllFiles();
 	}
